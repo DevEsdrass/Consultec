2e9fa66f9048ccf15515b1dfcad8f0831bcecd5c
Esdras Jales <131373468+DevEsdrass@users.noreply.github.com>
2024-11-08 20:04:55 -0300

    Add files via upload

---
+++ src/main/java/com/Agendamentos/Consultec/ConsultecApplication.java
@@ -0,0 +1,13 @@
+package com.Agendamentos.Consultec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Agendamentos/Consultec/ConsultecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Agendamentos.Consultec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
